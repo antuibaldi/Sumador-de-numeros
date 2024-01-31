c7ab766f8a7b1619c411555e42cdd8554185e2e4
Antu <122485403+antukung@users.noreply.github.com>
2024-01-31 20:55:24 -0300

    segundo commit

--- algoritmos.js
+++ algoritmos.js
@@ -1,8 +1,9 @@
-const numeroQueResultaLaSuma = 10;
+
 const cantidadDeProcedimientos = 6;
 
 let lista = [];
 
+let numeroQueResultaLaSuma = parseInt(prompt("Dame el primer numero que sumado dos veces se tiene que obtener"));
 let numero1 = parseInt(prompt("Dame un primer numero"));
 let numero2 = parseInt(prompt("Dame un segundo numero"));
 let numero3 = parseInt(prompt("Dame un tercer numero"));
@@ -20,12 +21,12 @@ function Comparador() {
         for (let j = 0; j < cantidadDeProcedimientos; j++) {
             suma= lista[i]+lista[j];
         if (suma===numeroQueResultaLaSuma) {
-            alert("La suma de dos números da 10");
+            alert("La suma de dos números da "+ numeroQueResultaLaSuma);
             return;
         }
     }
 }
 
     // Si no se encuentra ninguna coincidencia
-    alert("La suma de dos números no da 10");
+    alert("La suma de dos números no da " + numeroQueResultaLaSuma);
 }
